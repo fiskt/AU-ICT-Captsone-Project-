001a454759e562c7eacde7f5cd55759ff8d4beef
a-1924059 <a1924059@adelaide.edu.au>
2026-05-08 19:14:42 +0930

    restoring to previous

--- tennis-management-base/src/App.jsx
+++ tennis-management-base/src/App.jsx
@@ -1,59 +1,42 @@
-import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 import { COACH_SIDEBAR, PLAYER_SIDEBAR, TOPBAR } from './Components/SharedComponents';
 
-import Login from './pages/Login'
-import Register from './pages/Register'
+import Login from './pages/Login';
+import Register from './pages/Register';
 
-import CoachCalendar from './pages/CoachCalendar'
-import CoachDashboard from './pages/CoachDashboard'
-import PlayerProfile from './pages/PlayerProfile'
+import CoachCalendar from './pages/CoachCalendar';
+import CoachDashboard from './pages/CoachDashboard';
+import PlayerProfile from './pages/PlayerProfile';
 import DrillLibrary from './pages/DrillLibrary';
 import LoadTracking from './pages/LoadTracking';
 import Testing from './pages/Testing';
 import OtherUsers from './pages/OtherUsers';
 
-import PlayerCalendar from './pages/PlayerCalendar'
-import PlayerDashboard from './pages/PlayerDashboard'
-
-// create a Layout wrapper for your authenticated pages
-function DashboardLayout() {
-  return (
-    <div id="layout">
-      <COACH_SIDEBAR />
-      <TOPBAR />
-      <div id="main-content-wrapper">
-        <main id="main-content">
-          {/* This is where the nested routes will be rendered */}
-          <Outlet /> 
-        </main>
-      </div>
-    </div>
-  );
-}
+import PlayerCalendar from './pages/PlayerCalendar';
+import PlayerDashboard from './pages/PlayerDashboard';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* PUBLIC ROUTES (No Sidebar/Topbar) */}
-        {/* automatically redirect the root URL to the login page */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+      <div id="layout">
+        <COACH_SIDEBAR />
 
-        {/* PROTECTED ROUTES (Wrapped in DashboardLayout) */}
-        <Route element={<DashboardLayout />}>
-          <Route path="/CoachDashboard" element={<CoachDashboard />} />
-          <Route path="/CoachCalendar" element={<CoachCalendar />} />
-          <Route path="/PlayerProfile" element={<PlayerProfile />} />
-          <Route path="/DrillLibrary" element={<DrillLibrary />} />
-          <Route path="/Testing" element={<Testing />} />
-          <Route path="/OtherUsers" element={<OtherUsers />} />
+        <TOPBAR />
 
-          <Route path="/PlayerCalendar" element={<PlayerCalendar />} />
-          <Route path="/PlayerDashboard" element={<PlayerDashboard />} />
-        </Route>
-      </Routes>
+        <div id="main-content-wrapper">
+          <main id="main-content">
+            <Routes>
+              <Route path="/CoachDashboard" element={<CoachDashboard />} />
+              <Route path="/CoachCalendar" element={<CoachCalendar />} />
+              <Route path="/PlayerProfile" element={<PlayerProfile />} />
+              <Route path="/DrillLibrary" element={<DrillLibrary />} />
+              <Route path="/Testing" element={<Testing />} />
+              <Route path="/OtherUsers" element={<OtherUsers />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
