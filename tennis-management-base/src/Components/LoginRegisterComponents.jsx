1986acf4cc5686b96bcce48271f5b0a01f3e593f
a-1924059 <a1924059@adelaide.edu.au>
2026-05-10 10:21:37 +0930

    componenets for login register again

---
+++ tennis-management-base/src/Components/LoginRegisterComponents.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+import bgImage from '../assets/background.webp';
+import logo from '../assets/logo.png';
+
+export default function Layout() {
+  return (
+    <>
+      <div className="bg-image-wrapper">
+        <img src={bgImage} alt="Tennis court background" className="bg-image" />
+      </div>
+
+      <header>
+        <div className="logo-container">
+          <img src={logo} alt="HPT Logo" className="logo-image" />
+          <div className="logo-text">
+            <span className="bold-title"></span>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        {/* The Outlet renders whatever page (Login/Register) is currently active */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
