ae66ad8059eec93d52d8ef431509020acfba255e
a-1924059 <a1924059@adelaide.edu.au>
2026-05-10 11:35:55 +0930

    delete files

--- tennis-management-base/src/App.jsx
+++ tennis-management-base/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Layout from './Components/LoginRegisterComponents';
 import { COACH_SIDEBAR, TOPBAR } from './Components/SharedComponents';
 
 import Login from './pages/Login';
@@ -15,24 +16,10 @@ import OtherUsers from './pages/OtherUsers';
 function App() {
   return (
     <BrowserRouter>
-      <div id="layout">
-        <COACH_SIDEBAR />
-
-        <TOPBAR />
-
-        <div id="main-content-wrapper">
-          <main id="main-content">
-            <Routes>
-              <Route path="/CoachDashboard" element={<CoachDashboard />} />
-              <Route path="/CoachCalendar" element={<CoachCalendar />} />
-              <Route path="/PlayerProfile" element={<PlayerProfile />} />
-              <Route path="/DrillLibrary" element={<DrillLibrary />} />
-              <Route path="/Testing" element={<Testing />} />
-              <Route path="/OtherUsers" element={<OtherUsers />} />
-            </Routes>
-          </main>
-        </div>
-      </div>
+      <Routes>        
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
     </BrowserRouter>
   );
 }
