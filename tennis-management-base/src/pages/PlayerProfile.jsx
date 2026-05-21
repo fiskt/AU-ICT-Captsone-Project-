c4111df0e948476cad2ebdf21ff4d5280a6333f5
Amelia Fule <amelia.fule@icloud.com>
2026-05-21 15:04:27 +0930

    Work on Player Profile Page

--- tennis-management-base/src/pages/PlayerProfile.jsx
+++ tennis-management-base/src/pages/PlayerProfile.jsx
@@ -1,37 +1,48 @@
 import { DRILL_TYPE_GRAPH, EXERTION_GRAPH, NUM_DRILLS_GRAPH } from "../Components/LoadTrackingComponents";
+import { supabase } from '../supabaseClient';
+import { LOADING_OVERLAY } from '../Components/SharedComponents';
+import { useState } from "react";
+import '../App.css';
+import './PlayerProfile.css';
 
 export default function PlayerProfile() {
     return (
         <>
-            {/* Get info from database for things */}
-            <section> {/*Profile picture, name and buttons*/}
-                <div class="left">
-                    <div class="users-name">Player's First+Last name</div>
-                    <div class="user-type">Player</div>
+            <section class="playerProfilePage">
+                {/* Get info from database for things */}
+                <div class="playerNameSection"> {/*Profile picture, name and buttons*/}
                     <div class="profile-image">
-                        <img src="" alt="" />
+                        <img src="/src/assets/userPPDemo.png" alt="RF" /> {/* Players initials as alt + get details from DB*/}
+                    </div>
+                    <div class="left">
+
+                        <div class="users-name">
+                            <h3>Amelia Fule</h3> {/*get user's name and insert here */}
+                        </div>
+                        <div class="user-type">Player</div>
+
+                    </div>
+                    <div class="right">
+                        <ul class="listOfButtons">
+                            <li><btn class="profile-btn">Edit Profile Image</btn></li>
+                            <li><btn class="profile-btn">Update Details</btn></li>
+                        </ul>
                     </div>
                 </div>
-                <div class="right">
-                    <ul>
-                        <li><btn class="profile-btn">Edit Profile Image</btn></li>
-                        <li><btn class="profile-btn">Update Details</btn></li>
-                        <li><btn class="profile-btn">Update Profile</btn></li>
-                    </ul>
-                </div>
-            </section>
 
-            <section> {/*Feedback and stats content boxes */}
-                <div class="left">
+
+
+                <div class="feedbackAndStats"> {/*Feedback and stats content boxes */}
+
                     <div class="content-box">
                         <h2 class="content-header">PAST FEEDBACK</h2>
                         {/* put list of feedbacks here with title and date as buttons that open window with feedback */}
 
-                        <a><h2 class="content-seeAll">SEE ALL</h2></a>
-                        {/* button to display all past feedback in a list, on new page? window? with same functionality */}
+                    
+                        {/*scroll feature to see all instead of button*/}
                     </div>
-                </div>
-                <div class="right">
+
+
                     <div class="content-box">
                         <h2 class="content-header">STATS</h2>
 
@@ -43,33 +54,43 @@ export default function PlayerProfile() {
 
                         </ul>
                     </div>
+
+
                 </div>
 
-            </section>
-
-            <section>{/* Injuries and graphs? */}
-                <div class="left">
+                <div class="injuriesAndGraphs">{/* Injuries and graphs? */}
                     <div class="content-box">
                         <h2 class="content-header">INJURIES</h2>
                         {/* list of any relevant medical records here */}
+                       
+                    </div>
+
+                     <div class="content-box">
+                        <h2 class="content-header">SOMETHING</h2>
+                    
+                        
                     </div>
                 </div>
-            </section>
-  
 
-            <section>
-                <div class="left">
+
+                <div class="graphs">
+
                     <div class="content-box">
                         <h2 class="content-header">Rate of Exertion</h2>
-                        <EXERTION_GRAPH />
+                        <div class="graph">
+                            <EXERTION_GRAPH />
+                        </div>
                     </div>
-                </div>
-                <div class="right">
+
+
                     <div class="content-box">
                         <h2 class="content-header">Drills</h2>
-                        <NUM_DRILLS_GRAPH />
-                        <DRILL_TYPE_GRAPH />
+                        <div class="graph">
+                            <NUM_DRILLS_GRAPH />
+                            <DRILL_TYPE_GRAPH />
+                        </div>
                     </div>
+
                 </div>
             </section>
         </>
