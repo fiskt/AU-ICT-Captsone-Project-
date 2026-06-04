21f89c79be515c873a0e981d6183cbbd9f05bfa3
fiskt <a1911280@adelaide.edu.au>
2026-06-04 16:17:33 +0930

    supabase keys test

--- tennis-management-base/src/supabaseClient.js
+++ tennis-management-base/src/supabaseClient.js
@@ -4,3 +4,6 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey)
+
+console.log('URL:', import.meta.env.VITE_SUPABASE_URL);
+console.log('KEY exists:', !!import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
