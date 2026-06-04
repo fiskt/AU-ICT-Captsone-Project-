772cfa972bbe6c2714613ddc2a02545fcd17a4ea
fiskt <a1911280@adelaide.edu.au>
2026-06-04 16:51:12 +0930

    changed variable

--- tennis-management-base/src/supabaseClient.js
+++ tennis-management-base/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.SUPABASE_URL
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 
 console.log('URL:', supabaseUrl);
